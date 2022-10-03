5733091c6ef62df99b6da277a46f7730486f7ffd
Andrew-Goit <105640738+Andrew-Goit@users.noreply.github.com>
2022-10-03 22:58:26 +0300

    all 3 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,16 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+// const throttle = require('lodash.throttle');
 
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const LOCAL_TIME = 'videoplayer-current-time';
+const currentTime = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(timeUpDate, 1000));
 
-player.setCurrentTime(localStorage.getItem(LOCAL_TIME));
-
 function timeUpDate(data) {
-  localStorage.setItem(LOCAL_TIME, data.seconds);
+  localStorage.setItem(currentTime, data.seconds);
 }
+
+player.setCurrentTime(localStorage.getItem(currentTime));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onTextInput, 500));
+
+const userForm = {};
+
+function onTextInput(event) {
+//   const name = event.target.name;
+//   const data = event.target.value;
+//   userForm[name] = data;
+  userForm[event.target.name] = event.target.value
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(userForm));
+}
+
+getStorage();
+
+function getStorage() {
+  const savedData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+  if (!savedData) 
+  return;
+  if (savedData.email) {
+    email.value = savedData.email
+  }; 
+  if (savedData.message) {
+    message.value = savedData.message
+  };
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log({'email': email.value,'message': message.value});
+  event.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
 
