d4dd125af0869264e9d64e760a7b518be9f45ac3
Andrew-Goit <105640738+Andrew-Goit@users.noreply.github.com>
2022-10-03 10:30:35 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,30 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(SimpleLightbox);
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+function createGallery(galleryItems) {
+    return galleryItems.map(item =>
+    `<li>
+        <a class="gallery__item" 
+            href="${item.original}">
+            <img class="gallery__image" 
+            src="${item.preview}" 
+            alt="${item.description}" />
+        </a>
+    </li>`
+      ).join('');
+  }
+
+  galleryRef.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+  new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
